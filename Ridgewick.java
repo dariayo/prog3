57db9e665bd3c79b8f04ea4c7c0b67bed8d875c8
dariayo <90570083+dariayo@users.noreply.github.com>
2023-01-06 14:55:02 +0300

    Delete Ridgewick.java

--- Ridgewick.java
+++
@@ -1,6 +0,0 @@
-public class Ridgewick extends Person{
-    public Ridgewick(String name, int health, int age, Gender gender, Status status) {
-        super(name, health, age, gender, status);
-    }
-
-}
