f83f347e9ae2ef80bb89aedd7910548d34b4a98a
dariayo <90570083+dariayo@users.noreply.github.com>
2023-01-06 14:54:44 +0300

    Delete Pathologist.java

--- Pathologist.java
+++
@@ -1,5 +0,0 @@
-public class Pathologist extends Person{
-    public Pathologist(String name, int health, int age, Gender gender, Status status) {
-        super(name, health, age, gender, status);
-    }
-}
