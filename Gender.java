cc4808c56af06f1288d5366fd4646d2a63c6ca65
dariayo <90570083+dariayo@users.noreply.github.com>
2023-01-06 14:54:08 +0300

    Delete Gender.java

--- Gender.java
+++
@@ -1,9 +0,0 @@
-public enum Gender {
-    MAN("Мужчина"),
-    WOMEN("Женщина"),
-    CORPSE("Труп"),
-    ANIMAL("Животное");
-    private String NameGender;
-    Gender(String name){this.NameGender=name;}
-    public String getNameGender(){return NameGender;}
-}
