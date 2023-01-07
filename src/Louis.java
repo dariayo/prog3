79503e0a7f77fa052640b4c25c9211405a53f58f
dariayo <90570083+dariayo@users.noreply.github.com>
2023-01-07 23:48:52 +0300

    Update Louis.java

--- src/Louis.java
+++ src/Louis.java
@@ -8,17 +8,12 @@ public class Louis extends Person implements toArrive {
         if (item == Item.CASE){ //думает
             pascoe.age = (int) (pascoe.age + Math.random()*50);
             pascoe.getLocation(Places.LUDLOW);
-            //System.out.println(pascoe.name + " вырос в " + pascoe.places.getDescription());
-            //System.out.println("Возраст " + pascoe.name + " изменился с " + pascoe.age + " до " + age1);
 
             Animal.CAT.getLocation(Places.CEMETRY);
-            //System.out.println(pascoe.name+ " похоронил " + Animal.CAT.getTitle()+ " на " + places.getDescription());
             Animal.DOG.getLocation(Places.CEMETRY);
-            //System.out.println(pascoe.name+ " похоронил " + Animal.DOG.getTitle()+ " на " + places.getDescription());
 
             if (pascoe.health == 0) {
                 pascoe.status = status.DEAD;
-                //System.out.println(pascoe.name + " умер");
                 pascoe.name = "Dead " + pascoe.getName();
             }
         }
@@ -29,7 +24,6 @@ public class Louis extends Person implements toArrive {
     @Override
     public void arrive(Places places2) {
         this.places = getLocation(places2);
-        //System.out.println(name + " прибыл в " + this.places.getDescription());
     }
 
     public String eat(Item food) {
@@ -43,8 +37,7 @@ public class Louis extends Person implements toArrive {
     public void toThrew(Item item, Places places) {
         if (item == Item.TRASH & places == Places.TABLE) {
             item.getLocation(Places.BUCKET);
-            //this.places = Places.BUCKET;
-            System.out.println(name +" "+ item.getTitle() + " выбросил в " + item.places.getDescription());
+            //System.out.println(name +" "+ item.getTitle() + " выбросил в " + item.places.getDescription());
         }
     }
     public String toDrum(Item item){
