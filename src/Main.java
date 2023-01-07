efcffc955ec2a337a269b4fcbc9a8cdebd003c73
dariayo <90570083+dariayo@users.noreply.github.com>
2023-01-07 23:47:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,7 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
         Louis louis = new Louis();
-        louis.arrive(Places.CAFE);
-        louis.arrive(Places.CABINET);
-        System.out.println(louis.eat(Item.SANDWICH));
         final Pascoe pascoe = new Pascoe();
-        //louis.toThink(Item.CASE,pascoe);
-        louis.eat(Item.COCACOLA);
-        louis.toThrew(Item.TRASH,Places.TABLE);
-        louis.status=Status.NOTBAD;
-        louis.toDrum(Item.TABLE);
-        pascoe.toFly();
-        Funeralguy funeralguy = new Funeralguy();
-        funeralguy.arrive(Places.MORGUE);
-        Ridgewick ridgewick = new Ridgewick();
-        ridgewick.toOpen(pascoe);
-        Father father = new Father();
-        father.getLocation(Places.NEWARK);
-        father.arrive(Places.BERGENFIELD);
-        Pathologist pathologist = new Pathologist();
-        pathologist.toSuggest(pascoe);
-        System.out.println(pascoe.places);
         louis.toThink(Item.CASE,pascoe);
     }
 }
