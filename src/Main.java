cbddb31ffc1ee05494e5361727eae612fb3d9e77
dariayo <90570083+dariayo@users.noreply.github.com>
2023-01-08 21:37:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Louis louis = new Louis();
         Pascoe pascoe = new Pascoe();
-        Pascoe deadpascoe = new Pascoe();
-        louis.toThink(Item.CASE,deadpascoe); // Так возраст у Паскоу меняться не будет
+        Pascoe pascoeInMind = new Pascoe();
+        louis.toThink(Item.CASE,pascoeInMind); // Так возраст у Паскоу меняться не будет
     }
 }
