3fa1b973632ae12d0f27f6c5c7638a0e4cdb0961
dariayo <90570083+dariayo@users.noreply.github.com>
2023-01-15 14:06:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
 public class Main {
     public static void main(String[] args) {
+
         Louis louis = new Louis();
+        louis.arrive(Places.CAFE);
+        louis.arrive(Places.CABINET);
+        System.out.println(louis.eat(Item.SANDWICH));
+
+        louis.eat(Item.COCACOLA);
+        louis.toThrew(Item.TRASH, Places.TABLE);
+        louis.status = Status.NOTBAD;
+        louis.toDrum(Item.TABLE);
+
         Pascoe pascoe = new Pascoe();
-        Pascoe pascoeInMind = new Pascoe();
-        louis.toThink(Item.CASE,pascoeInMind); // Так возраст у Паскоу меняться не будет
+        //Pascoe pascoeInMind = new Pascoe();
+        louis.toThink(Item.CASE, pascoe);
+        pascoe.toFly();
+
+        Funeralguy funeralguy = new Funeralguy();
+        funeralguy.arrive(Places.MORGUE);
+
+        Ridgewick ridgewick = new Ridgewick();
+        ridgewick.toOpen(pascoe);
+
+        Father father = new Father();
+        father.getLocation(Places.NEWARK);
+        father.arrive(Places.BERGENFIELD);
+
+        Pathologist pathologist = new Pathologist();
+        pathologist.toSuggest(pascoe);
+        //louis.toThink(Item.CASE,pascoeInMind);
     }
 }
