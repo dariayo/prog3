c83437fd4877bf20e71d665adcc86d3764f3c714
dariayo <90570083+dariayo@users.noreply.github.com>
2023-01-15 15:09:17 +0300

    Update Pathologist.java

--- src/Pathologist.java
+++ src/Pathologist.java
@@ -1,9 +1,18 @@
-public class Pathologist extends Person {
+public class Pathologist extends Person implements Sayable {
     public Pathologist() {
         super("Патологоанатом",100,30,Gender.MAN,Status.CALM);
     }
     public Places toSuggest(Pascoe pascoe){
         return pascoe.getLocation(Places.DISCO);
+    }
+    
+    @Override
+    public String SayAbout(String personName){
+        if (personName.equals("Луис")) {
+            return "У нас его нет";
+        } else {
+            return "Даже не знаю, что и сказать";
+        }
     }
 
 }
