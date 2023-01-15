1967f5729fa862700ba52a5ebde997b5b7bc7b58
dariayo <90570083+dariayo@users.noreply.github.com>
2023-01-15 14:30:53 +0300

    Update Status.java

--- src/Status.java
+++ src/Status.java
@@ -1,20 +1,22 @@
 public enum Status {
     DEAD("Умер"),
-    NOTBAD("в порядке"),
-    OPEN("вскрытый"),
-    SEARCH("в поиске"),
-    CALM("Спокойный");
+    NOTBAD("В порядке"),
+    SHOCKED("Удивлен"),
+    OPEN("Вскрыт"),
+    SEARCH("В поиске"),
+    CALM("Спокоен");
     private String title;
+    protected Status status;
 
     Status(String title) {
         this.title = title;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
     public Status getStatus(Status status) {
         return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 }
