3048c55a5b4a6609bcb2ab6d71bd9825b64b7faa
dariayo <90570083+dariayo@users.noreply.github.com>
2023-01-15 12:04:59 +0300

    Update Louis.java

--- src/Louis.java
+++ src/Louis.java
@@ -1,13 +1,27 @@
-public class Louis extends Person implements toArrive {
+public class Louis extends Person implements toArrive, Sayable {
     public Louis() {
-        super("Луис",45, 30, Gender.MAN, Status.CALM);
+        super("Луис", 45, 30, Gender.MAN, Status.CALM);
         System.out.println("Персонаж " + name + " теперь существует");
     }
 
-    public void toThink(Item item,Pascoe pascoe){
-        if (item == Item.CASE){ //думает
-            pascoe.age = (int) (pascoe.age + Math.random()*50);
-            pascoe.getLocation(Places.LUDLOW);
+    @Override
+    public String SayAbout(String personName) {
+        if (personName.equals("Патологоанатом")) {
+            return "У вас наш студент, Виктор Паскоу";
+        }
+        if (personName.equals("Патологоанатом") && this.status == Status.SHOCKED) {
+            return "Что?";
+        } else {
+            return "Даже не знаю, что и сказать";
+        }
+    }
+
+    public void toThink(Item item, Pascoe pascoe) {
+        if (item == Item.CASE) {
+            pascoe.age = (int) (pascoe.age + Math.random() * 50);
+            pascoe.setLocation(Places.LUDLOW);
+            this.status.setStatus(Status.SEARCH);
+            //System.out.println(pascoe.age);
 
             Animal.CAT.getLocation(Places.CEMETRY);
             Animal.DOG.getLocation(Places.CEMETRY);
@@ -16,14 +30,15 @@ public class Louis extends Person implements toArrive {
                 pascoe.status = status.DEAD;
                 pascoe.name = "Dead " + pascoe.getName();
             }
-        }
-        else if(item == null || pascoe!=pascoe){
-            //System.out.println("Я ни о чем не думаю");
+        } else if (item == null || pascoe != pascoe) {
+            System.out.println("Я ни о чем не думаю");
         }
     }
+
     @Override
-    public void arrive(Places places2) {
-        this.places = getLocation(places2);
+    public void arrive(Places places) {
+        this.places = getLocation(places);
+        //System.out.println(name + " прибыл в " + this.places.getDescription());
     }
 
     public String eat(Item food) {
@@ -31,16 +46,18 @@ public class Louis extends Person implements toArrive {
             health = health + 10;
             return name + " съел " + food.getTitle();
         }
-            return "Я не голодный";
-        }
+        return "Я не голодный";
+    }
 
     public void toThrew(Item item, Places places) {
         if (item == Item.TRASH & places == Places.TABLE) {
             item.getLocation(Places.BUCKET);
-            //System.out.println(name +" "+ item.getTitle() + " выбросил в " + item.places.getDescription());
+            //System.out.println(name + " " + item.getTitle() + " выбросил в " + item.places.getDescription());
         }
     }
-    public String toDrum(Item item){
-        return name+" побарабанил по " + item.getTitle();
+
+    public String toDrum(Item item) {
+        return name + " побарабанил по " + item.getTitle();
     }
 }
+
