8c919182a959774b32406577856de5d58b103341
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-01-21 18:12:58 +0300

    persons

--- src/persons/Louis.java
+++ src/persons/Louis.java
@@ -1,7 +1,7 @@
-package Person;
+package persons;
 
-import Enum.*;
-import Interfaces.*;
+import enums.*;
+import interfaces.*;
 
 public class Louis extends Person implements ToArrive, Sayable {
     public Louis() {
