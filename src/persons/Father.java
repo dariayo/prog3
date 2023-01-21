549726832a7b982b715cee356dbe12fa20715b91
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-01-21 18:12:30 +0300

    persons

--- src/persons/Father.java
+++ src/persons/Father.java
@@ -1,7 +1,7 @@
-package Person;
+package persons;
 
-import Enum.*;
-import Interfaces.*;
+import enums.*;
+import interfaces.*;
 
 public class Father extends Person implements ToArrive, Sayable {
     public Father() {
