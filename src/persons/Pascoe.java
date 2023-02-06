ab497aad2a861052fee672926311b84576301bd6
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-02-06 18:35:14 +0300

    Update Pascoe.java

--- src/persons/Pascoe.java
+++ src/persons/Pascoe.java
@@ -10,8 +10,11 @@ public class Pascoe extends Person implements ToArrive {
     }
 
     @Override
-    public void arrive(Places places) {
+    public void arrive(Places places) throws IllegalLocationException {
         this.places = places;
+        if (places==Places.BUCKET || places==Places.TABLE){
+            throw new IllegalLocationException(" невозможная локация для человека");
+        }
         System.out.println(name + " прибыл в " + this.places.getDescription());
     }
 
