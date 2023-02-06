dda59cc5c1c271a4e1bac15e12a9c4533ef17297
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-02-06 18:30:47 +0300

    Update Funeralguy.java

--- src/persons/Funeralguy.java
+++ src/persons/Funeralguy.java
@@ -1,6 +1,7 @@
 package persons;
 
 import enums.*;
+import exceptions.IllegalLocationException;
 import interfaces.*;
 
 public class Funeralguy extends Person implements ToArrive {
@@ -9,8 +10,11 @@ public class Funeralguy extends Person implements ToArrive {
     }
 
     @Override
-    public void arrive(Places places) {
+    public void arrive(Places places) throws IllegalLocationException {
         this.places = getLocation(places);
+        if (places==Places.BUCKET || places==Places.TABLE){
+            throw new IllegalLocationException(" невозможная локация для человека");
+        }
         System.out.println(name + " прибыл в " + this.places.getDescription());
     }
 }
