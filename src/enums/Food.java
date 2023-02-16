ce00ee578b8d4976cd1db24e19d9bc845cfb64dc
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-02-16 13:13:34 +0300

    Add files via upload

---
+++ src/enums/Food.java
@@ -0,0 +1,18 @@
+package enums;
+
+public enum Food {
+    COCACOLA("кока-кола",250),
+    BEER("пиво",110),
+    SANDWICH("сэндвич с тунцом",500);
+    private String title;
+    public int calories;
+
+    public String getTitle() {
+        return title;
+    }
+
+    Food(String title, int calories) {
+        this.title = title;
+        this.calories = calories;
+    }
+}
