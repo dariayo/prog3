ab3f45c572459d9b31b836ac02342c75ec6c87a4
Daria Shevchenko <90570083+dariayo@users.noreply.github.com>
2023-02-16 13:12:58 +0300

    Update Item.java

--- src/enums/Item.java
+++ src/enums/Item.java
@@ -1,21 +1,23 @@
 package enums;
 
 public enum Item {
-    COCACOLA("кока-кола"),
-    SANDWICH("сэндвич с тунцом"),
-    TABLE("стол"),
-    BEER("пиво"),
-    CASE("дело"),
-    TRASH("мусор");
+    TABLE("стол","имеет аккустику",120),
+    CASE("дело","сделано из бумаги",15),
+    BUCKET("ведро","красного цвета",50),
+    TRASH("мусор","можно утилизировать",25);
     private String title;
     public Places places;
+    public String property;
+    public int size;
 
-    public Places getLocation(Places places) {
+    public Places changeLocation(Places places) {
         return this.places = places;
     }
 
-    Item(String title) {
+    Item(String title, String property,int size) {
         this.title = title;
+        this.property= property;
+        this.size=size;
     }
 
     public String getTitle() {
